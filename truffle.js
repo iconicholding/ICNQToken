af281550369036a4eae4eb6e2b422cbcd9cddef5
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-10-04 11:34:52 +0200

    Install dev dependencies.

--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+require('babel-register')
+require('babel-polyfill')
+process.env.NODE_ENV = 'test'
+process.env.BABEL_ENV = 'test'
+
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
